4bcf41930c140f671f8c5382ed24a046cbe79e2f
ahmadzip <ahmad.5220411084@student.uty.ac.id>
2023-11-18 05:12:34 +0700

    Dark mode button to icon

--- src/app/component/ThemeSwitcher.tsx
+++ src/app/component/ThemeSwitcher.tsx
@@ -1,4 +1,5 @@
-"use client";
+"use effect";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
@@ -19,7 +20,7 @@ export const ThemeSwitcher = () => {
       className={`w-fit absolute right-5 top-2 p-2 rounded-md hover:scale-110 active:scale-100 duration-200 bg-slate-200 dark:bg-[#212933]`}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
-      {theme === "light" ? "Dark" : "Light"}
+      {theme === "light" ? <FaMoon /> : <FaSun />}
     </button>
   );
 };
